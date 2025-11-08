0032ade93f83f454d21e17753f30a538509b0a0e
harsh <nh1750501@gmail.com>
2025-11-08 21:16:32 +0530

    Added conditions for protected kroute

--- cloudinary-saas/middleware.ts
+++ cloudinary-saas/middleware.ts
@@ -1,26 +1,29 @@
-import { clerkMiddleware,createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 // clerkMatcher --> It "matches" URL paths and tells your middleware which routes to make public and which to protect.
 
-const isPublicroute = createRouteMatcher([
-  "/login",
-  "/signup",
-  "/",
-  "/home",
-])
+const isPublicroute = createRouteMatcher(["/login", "/signup", "/", "/home"]);
 
-const isPublicApiRoute = createRouteMatcher([
-  "/api/videos",
+const isPublicApiRoute = createRouteMatcher(["/api/videos"]);
 
-])
+export default clerkMiddleware((auth, req) => {
+  const { userId } = auth();
+  const currentUrl = new URL(req.url);
+  const isAccessingHomepage = currentUrl.pathname === "/home";
 
-export default clerkMiddleware();
+  const isApirequest = currentUrl.pathname.startsWith("/api");
+
+  if (userId && isPublicApiRoute(req) && !isAccessingHomepage) {
+    return NextResponse.redirect(new URL("/home", req.url));
+  }
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
